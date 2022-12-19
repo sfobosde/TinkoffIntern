b873ada92f593cd6c78c582fc3abc89e3705f4e3
Grigory Vasilev <grishavasilev62@gmail.com>
2022-12-19 22:25:18 +0300

    Attach some changes

--- DetermineNameColor.py
+++ DetermineNameColor.py
@@ -1,6 +1,6 @@
 # Ввод текста.
-def inputData():
-    # Количество символов в названии отдела.
+def input_data():
+    # Symbol count.
     symbol_count: int
 
     # Название отдела.
@@ -9,11 +9,15 @@ def inputData():
     # Идея раскраски.
     coloring_idea: str
 
+    # Контроль ввода и обработка исключения.
     try:
         symbol_count = int(input("Введите количество букв в названии отдела:"))
     except ValueError:
         print("Необходимо ввести число.")
+        symbol_count = 0
 
     department_name = input("Введите название отдела:")
 
     coloring_idea = input("Введите идею раскраски:")
+
+    return symbol_count, department_name, coloring_idea

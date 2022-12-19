3d0f69fcaaaef3d8227d5c5050d3bec79e50be68
Grigory Vasilev <grishavasilev62@gmail.com>
2022-12-19 22:42:47 +0300

    refactor

--- Solution/DetermineNameColor.py
+++ Solution/DetermineNameColor.py
@@ -10,14 +10,24 @@ def input_data():
     coloring_idea: str
 
     # Контроль ввода и обработка исключения.
+    # Используется проброска исключений.
     try:
         symbol_count = int(input("Введите количество букв в названии отдела:"))
     except ValueError:
         print("Необходимо ввести число.")
-        symbol_count = 0
+        raise Exception("Ошибка вводимых данных")
+    except TypeError:
+        print("Необходимо ввести целое число.")
+        raise Exception("Ошибка вводимых данных")
 
     department_name = input("Введите название отдела:")
 
     coloring_idea = input("Введите идею раскраски:")
 
     return symbol_count, department_name, coloring_idea
+
+
+def determine_coloring_mistakes(symbol_count: int, department_name: str, coloring_idea: str):
+    pass
+
+
--- Solution/Tests/DetermineColorTest.py
+++ Solution/Tests/DetermineColorTest.py
@@ -1,8 +1,8 @@
 # Автоматическое тестировние для раскрашивания логотипа.
 from unittest import TestCase
-
+import Solution.DetermineNameColor as determine_name_color
 
 # Algorithms and Data Structures
 # BBBBBBBBBB BYB YYYY BBBBBBBBBB
-class DetermineColoringTest(TestCase):
+class TestDetermineColoring(TestCase):
     pass
